524fbda7e52183bd5f8768f28347946e1512fe04
itsahadul99 <itsahadul999@gmail.com>
2024-03-25 19:11:04 +0600

    basic setup complete for project starting

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Testing .....</h1>
+            <button className="btn btn-primary">Btn</button>
+        </div>
+    );
+};
+
+export default MainLayout;
